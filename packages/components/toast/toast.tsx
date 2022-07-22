f21096928194f076a747e7aaf83931118e8a285d
lilinhui <yy0088@wb.com>
2022-07-22 13:57:45 +0800

    test: publish ci

--- packages/components/toast/toast.tsx
+++ packages/components/toast/toast.tsx
@@ -2,10 +2,20 @@ import { FC } from 'react'
 import { Text, View } from '@tarojs/components'
 import { LiToastProps } from '@/types/toast'
 
-const Toast: FC<LiToastProps> = () => (
-  <View>
-    <Text>11112222</Text>
-  </View>
+const Toast: FC<LiToastProps> = ({ isOpened, icon, text }) => (
+  <>
+    <View>
+      <Text>{text}</Text>
+    </View>
+    {isOpened && (
+      <View>
+        <Text>11112222</Text>
+        <View>
+          <Text>{icon}</Text>
+        </View>
+      </View>
+    )}
+  </>
 )
 
 export default Toast
